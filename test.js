c347d80db7dbeea2e3ffa8508547c30b8aaac71f
Manuel Cangas <79407122+ManuelCangas@users.noreply.github.com>
2024-05-31 11:39:12 -0400

    Update test.js
    Solución final al ejercicio

--- test.js
+++ test.js
@@ -1,12 +1,26 @@
-/** Contador de Caracteres
-Descripción:
-Escribe una función o método que tome como entrada una cadena de texto y devuelva un
-diccionario/histograma que muestre la frecuencia de cada carácter en el texto, ignorando espacios.
-
-Ejemplo:
-Si el texto es "la casa es grande", el resultado debería ser un diccionario 
-como {"l": 1, "a": 4, "c": 1, "s": 2, "e": 2, "g": 1, "r": 1, "n": 1, "d": 1}. */
-
 //iniciamos una variable que posea nuestra oración
 let oracion = "la casa es grande";
+let resultado = contarCaracteres(oracion);
+console.log(resultado);
 
+function contarCaracteres(oracion) {
+    //Objeto vacío para almacenar las frecuencias
+    let frecuencias = {};
+    //Recorremos cada caracter de la cadena de texto
+    for (let i = 0; i < oracion.length; i++) {
+        let caracter = oracion[i];
+        // Ignoramos los espacios
+        if (caracter === ' ') {
+            continue;
+        }
+        // Si el carácter ya está en el objeto, incrementamos su frecuencia
+        if (frecuencias[caracter]) {
+            frecuencias[caracter]++;
+        } else {
+            // Si el carácter no está en el objeto, lo añadimos con frecuencia 1
+            frecuencias[caracter] = 1;
+        }
+    }
+    // Devolvemos el diccionario con las frecuencias
+    return frecuencias;
+}
